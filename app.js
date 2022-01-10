25738ce2f4f590e7787d10a3347b604bcb0ce567
Mamun-swe <mhmamun166009@gmail.com>
2022-01-10 16:11:04 +0600

    chyange port

--- app.js
+++ app.js
@@ -76,7 +76,7 @@ if (cluster.isMaster) {
         })
 
     // App Port
-    const port = process.env.PORT || 5000
+    const port = process.env.PORT || 5001
     app.listen(port, () => {
         console.log(`App running on ${port} port`)
     })
